bbd1d3230b4ca494e8e4f959e32dc8933df55124
zhangchengyao <tc2002618@126.com>
2015-10-15 09:39:31 +0800

    整理

--- server/src/main/java/edu/nju/expressMgmtSys/server/Server.java
+++
@@ -1,9 +0,0 @@
-package edu.nju.expressMgmtSys.server;
-
-public class Server {
-
-    public static void main(String[] args) {
-        RMIHelper.init();
-        System.out.println("Server started!");
-    }
-}
