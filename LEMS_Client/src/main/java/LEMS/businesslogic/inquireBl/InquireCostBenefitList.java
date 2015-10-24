a7ede2f816754ef7a0e39bf2eef92faa174c7332
zhangchengyao <tc2002618@126.com>
2015-10-24 20:55:57 +0800

    删除

--- LEMS_Client/src/main/java/LEMS/businesslogic/inquireBl/InquireCostBenefitList.java
+++
@@ -1,7 +0,0 @@
-package LEMS.businesslogic.inquireBl;
-
-import LEMS.businesslogicservice.inquireBlService.InquireCostBenefitListService;
-
-public class InquireCostBenefitList implements InquireCostBenefitListService {
-
-}
--- LEMS_Client/src/main/java/LEMS/businesslogicservice/inquireBlService/InquireDiaryService.java
+++
@@ -1,7 +0,0 @@
-package LEMS.businesslogicservice.inquireBlService;
-
-import LEMS.vo.DiaryVO;
-
-public interface InquireDiaryService {
-	public DiaryVO getDiary();
-}
