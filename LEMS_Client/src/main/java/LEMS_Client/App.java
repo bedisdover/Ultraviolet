14615f58c9c289a0634f0e446d513492d84c9a5c
zhangchengyao <tc2002618@126.com>
2015-10-24 20:50:40 +0800

    maven project

---
+++ LEMS_Client/src/main/java/LEMS_Client/App.java
@@ -0,0 +1,13 @@
+package LEMS_Client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
