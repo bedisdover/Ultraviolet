ee8ed276a994cf1447bfcd67b3473039ece16417
bedisdover <sym14@software.nju.edu.cn>
2015-10-25 11:41:08 +0800

    新建PO

---
+++ LEMS_Client/src/main/java/LEMS/vo/financevo/DocumentVO.java
@@ -0,0 +1,17 @@
+package LEMS.vo.financevo;
+
+/**
+ * @author 宋益明
+ * 
+ * 所有单据的基类
+ */
+public abstract class DocumentVO {
+	/**
+	 * 单据生成日期
+	 */
+	private String date;
+
+	public String getDate() {
+		return date;
+	}
+}
