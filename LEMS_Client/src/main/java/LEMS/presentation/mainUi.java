dc814f9837df181c29ff4f2e180c14c094277645
zhangchengyao <tc2002618@126.com>
2015-10-26 13:15:20 +0800

    界面略微修改

--- LEMS_Client/src/main/java/LEMS/presentation/mainUi.java
+++ LEMS_Client/src/main/java/LEMS/presentation/mainUi.java
@@ -9,8 +9,12 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import javax.swing.JList;
+import javax.swing.JComboBox;
 
 public class mainUi extends JFrame {
 
@@ -37,6 +41,7 @@ public class mainUi extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	@SuppressWarnings("rawtypes")
 	public mainUi() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 820, 570);
@@ -46,21 +51,21 @@ public class mainUi extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel label = new JLabel("账号");
-		label.setBounds(143, 133, 102, 41);
+		label.setBounds(142, 80, 102, 41);
 		contentPane.add(label);
 		
 		JLabel label_1 = new JLabel("密码");
-		label_1.setBounds(143, 211, 90, 41);
+		label_1.setBounds(142, 158, 90, 41);
 		contentPane.add(label_1);
 		
 		textField = new JTextField();
-		textField.setBounds(292, 140, 200, 27);
+		textField.setBounds(291, 87, 200, 27);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(292, 218, 200, 27);
+		textField_1.setBounds(291, 165, 200, 27);
 		contentPane.add(textField_1);
 		
 		JButton button = new JButton("登录");
@@ -70,5 +75,16 @@ public class mainUi extends JFrame {
 		JButton button_2 = new JButton("查询物流信息");
 		button_2.setBounds(277, 447, 245, 27);
 		contentPane.add(button_2);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(327, 226, 116, 32);
+		contentPane.add(comboBox);
+		comboBox.addItem("管理员");
+		comboBox.addItem("总经理");
+		comboBox.addItem("财务人员");
+		comboBox.addItem("快递员");
+		comboBox.addItem("营业厅业务员");
+		comboBox.addItem("中转中心业务员");
+		comboBox.addItem("仓库管理人员");
 	}
 }
