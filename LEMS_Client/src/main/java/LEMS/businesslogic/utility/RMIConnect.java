47105a6aca7cece038a2df29a1dbdbc3958e39e1
zhangchengyao <tc2002618@126.com>
2015-12-08 21:50:17 +0800

    》‘’

--- LEMS_Client/src/main/java/LEMS/businesslogic/utility/RMIConnect.java
+++ LEMS_Client/src/main/java/LEMS/businesslogic/utility/RMIConnect.java
@@ -1,5 +1,5 @@
 package LEMS.businesslogic.utility;
 
 public class RMIConnect {
-	public static final String RMI="rmi://172.29.150.3:1099/data";
+	public static final String RMI="rmi://localhost:1099/data";
 }
--- LEMS_Server/src/main/java/LEMS/data/Connect.java
+++ LEMS_Server/src/main/java/LEMS/data/Connect.java
@@ -10,7 +10,7 @@ import LEMS.data.userdata.UserData;
 
 public class Connect {
 	public static final String DBDRIVER = "org.gjt.mm.mysql.Driver";
-	public static final String DBURL = "jdbc:mysql://172.29.150.3:3306/mldn";
+	public static final String DBURL = "jdbc:mysql://localhost:3306/mldn";
 	public static final String DBUSER = "root";
 	public static final String DBPASS = "admin";
 
