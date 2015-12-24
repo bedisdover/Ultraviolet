80e284d5a9ba5f3baf79f74ef20cdec03177a51a
zhangchengyao <tc2002618@126.com>
2015-12-24 22:42:02 +0800

    rmi ip修改

--- LEMS_Client/src/main/java/LEMS/businesslogic/utility/RMIConnect.java
+++ LEMS_Client/src/main/java/LEMS/businesslogic/utility/RMIConnect.java
@@ -6,23 +6,23 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 public class RMIConnect {
-	public static final String RMI="rmi://114.212.42.237:1099/data";
-//	public static String RMI;
-//	
-//	
-//	static {
-//		try {
-//			//创建XML读取器
-//			SAXReader reader = new SAXReader();
-//			//读取
-//			Document document = reader.read("config.xml");
-//			//获取根目录
-//			Element system = document.getRootElement();
-//			Element connect = system.element("Connect");
-//
-//			RMI = connect.attributeValue("IP");
-//		} catch (DocumentException e) {
-//			e.printStackTrace();
-//		}
-//	}
+//	public static final String RMI="rmi://114.212.42.237:1099/data";
+	public static String RMI;
+	
+	
+	static {
+		try {
+			//创建XML读取器
+			SAXReader reader = new SAXReader();
+			//读取
+			Document document = reader.read("config.xml");
+			//获取根目录
+			Element system = document.getRootElement();
+			Element connect = system.element("Connect");
+
+			RMI = connect.attributeValue("IP");
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+	}
 }
