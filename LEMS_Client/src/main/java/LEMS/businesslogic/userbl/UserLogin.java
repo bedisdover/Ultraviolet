4ff9f3756af9b2e2fd135a7005f2a7160b1d5dfe
zhangchengyao <tc2002618@126.com>
2015-12-24 22:37:03 +0800

    登录网络连接检查

--- LEMS_Client/src/main/java/LEMS/businesslogic/userbl/UserLogin.java
+++ LEMS_Client/src/main/java/LEMS/businesslogic/userbl/UserLogin.java
@@ -3,14 +3,14 @@ package LEMS.businesslogic.userbl;
 import LEMS.vo.uservo.UserVO;
 
 public class UserLogin {
-	public UserVO login(String id, String password) {
+	public UserVO login(String id, String password) throws Exception {
 		User user = new User(id, password);
 		UserVO uvo = null;
 		try {
 			uvo = user.getUser();
 		} catch (Exception e) {
 			// TODO 提示异常
-			e.printStackTrace();
+			throw e;
 		}
 		return uvo;
 	}
--- LEMS_Client/src/main/java/LEMS/presentation/LoginUi.java
+++ LEMS_Client/src/main/java/LEMS/presentation/LoginUi.java
@@ -71,79 +71,56 @@ public class LoginUi extends JPanel {
 				final String password = textPassword.getText();
 				UserLogin ul = new UserLogin();
 				UserVO userVO = null;
-				userVO=ul.login(name, password);
-//				switch (role) {
-				//TODO 更改选项，添加提示“网络连接”
-//				case "管理员":
-//					userVO = ul.login(name, password, UserRole.Manager);
-//					break;
-//				case "总经理":
-//					userVO = ul.login(name, password, UserRole.GeneralManager);
-//					break;
-//				case "仓库管理人员":
-//					userVO = ul.login(name, password, UserRole.StoreManager);
-//					break;
-//				case "财务人员":
-//					userVO = ul.login(name, password, UserRole.FinanceClerk);
-//					break;
-//				case "快递员":
-//					userVO = ul.login(name, password, UserRole.Courier);
-//					break;
-//				case "营业厅业务员":
-//					userVO = ul.login(name, password, UserRole.BusinessClerk);
-//					break;
-//				case "中转中心业务员":
-//					userVO = ul.login(name, password, UserRole.TransferClerk);
-//					break;
-//				default:
-//					break;
-//				}
-				
-				if (userVO == null) {
-					JOptionPane.showMessageDialog(LoginUi.this, "账号或密码错误！");
-				} else {
-					switch (userVO.getRole()) {
-					// 登录后跳转到管理员界面
-					case Manager:
-						mainFrame.setContentPane(new ManagerUi(mainFrame,userVO));
-						repaint();
-						break;
-					//登录后跳转到仓库管理员界面
-					case StoreManager:
-						mainFrame.setContentPane(new StoreManagerUi(mainFrame,userVO));
-						repaint();
-						break;
-					//登录后跳转到总经理界面
-					case GeneralManager:
-						mainFrame.setContentPane(new GeneralManagerUi(mainFrame,userVO));
-						repaint();
-						break;
-					//登录后跳转到财务人员界面
-					case FinanceClerk:
-						mainFrame.setContentPane(new FinancialStaffUi(mainFrame,userVO));
-						repaint();
-						break;
-					//登录后跳转到快递员界面
-					case Courier:
-						mainFrame.setContentPane(new CourierUi(mainFrame,userVO));
-						repaint();
-						break;
-					//登录后跳转到营业厅业务员界面
-					case BusinessClerk:
-						mainFrame.setContentPane(new BusinessClerkUi(mainFrame,userVO));
-						repaint();
-						break;
-					//登录后跳转到中转中心员界面
-					case TransferClerk:
-						mainFrame.setContentPane(new TransferClerkUi(mainFrame,userVO));
-						repaint();
-						break;
-					default:
-						break;
+				try {
+					userVO=ul.login(name, password);
+					if (userVO == null) {
+						JOptionPane.showMessageDialog(LoginUi.this, "账号或密码错误！");
+					} else {
+						switch (userVO.getRole()) {
+						// 登录后跳转到管理员界面
+						case Manager:
+							mainFrame.setContentPane(new ManagerUi(mainFrame,userVO));
+							repaint();
+							break;
+						//登录后跳转到仓库管理员界面
+						case StoreManager:
+							mainFrame.setContentPane(new StoreManagerUi(mainFrame,userVO));
+							repaint();
+							break;
+						//登录后跳转到总经理界面
+						case GeneralManager:
+							mainFrame.setContentPane(new GeneralManagerUi(mainFrame,userVO));
+							repaint();
+							break;
+						//登录后跳转到财务人员界面
+						case FinanceClerk:
+							mainFrame.setContentPane(new FinancialStaffUi(mainFrame,userVO));
+							repaint();
+							break;
+						//登录后跳转到快递员界面
+						case Courier:
+							mainFrame.setContentPane(new CourierUi(mainFrame,userVO));
+							repaint();
+							break;
+						//登录后跳转到营业厅业务员界面
+						case BusinessClerk:
+							mainFrame.setContentPane(new BusinessClerkUi(mainFrame,userVO));
+							repaint();
+							break;
+						//登录后跳转到中转中心员界面
+						case TransferClerk:
+							mainFrame.setContentPane(new TransferClerkUi(mainFrame,userVO));
+							repaint();
+							break;
+						default:
+							break;
+						}
+
 					}
-
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(LoginUi.this, "请检查网络连接！");
 				}
-
+			
 			}
 		});
 		// 点击返回按钮
