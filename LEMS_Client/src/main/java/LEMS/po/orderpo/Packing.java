b3f18c100f733082124050aec71d8e729db82d1c
bedisdover <sym14@software.nju.edu.cn>
2015-12-31 11:28:26 +0800

    meijup

--- LEMS_Client/src/main/java/LEMS/po/orderpo/Packing.java
+++ LEMS_Client/src/main/java/LEMS/po/orderpo/Packing.java
@@ -1,6 +1,7 @@
 package LEMS.po.orderpo;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  * @author 宋益明
@@ -12,4 +13,21 @@ public enum Packing implements Serializable {
 	Wooden, // 木箱
 	Bag, // 快递袋
 	Other// 其它
+	;
+	
+	private static HashMap<String, Packing> typeList = new HashMap<>();
+	
+	static {
+		typeList.put("快递袋", Packing.Bag);
+		typeList.put("纸箱", Packing.Carton);
+		typeList.put("木箱", Packing.Wooden);
+		typeList.put("其它", Packing.Other);
+	}
+	
+	/**
+	 * 将包装类型的中文转换为对应的包装类型
+	 */
+	public static Packing transfer(String type) {
+		return typeList.get(type);
+	}
 }
