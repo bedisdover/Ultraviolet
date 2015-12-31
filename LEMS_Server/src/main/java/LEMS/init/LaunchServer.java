5f6be293b464cd404d754218ee4725efb9432757
zhangchengyao <tc2002618@126.com>
2015-12-31 19:48:01 +0800

    #03 服务器加了个界面

--- LEMS_Server/src/main/java/LEMS/init/LaunchServer.java
+++ LEMS_Server/src/main/java/LEMS/init/LaunchServer.java
@@ -2,8 +2,14 @@ package LEMS.init;
 
 import java.rmi.registry.LocateRegistry;
 
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
 import LEMS.data.factoryImpl.DatabaseFactoryImpl;
 import LEMS.dataservice.factory.DatabaseFactory;
+import java.awt.Rectangle;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 /**
  * @author 章承尧
@@ -12,10 +18,20 @@ import LEMS.dataservice.factory.DatabaseFactory;
 public class LaunchServer {
 	public static final int PORT=1099;
 	public static void main(String[] args){
+		JFrame frame=new JFrame();
+		frame.setBounds(new Rectangle(500, 400, 200, 150));
+		JTextField field=new JTextField();
+		field.setEditable(false);
+		field.setHorizontalAlignment(SwingConstants.CENTER);
+		field.setFont(new Font("宋体", Font.PLAIN, 40));
+		frame.getContentPane().add(field);
+		frame.setVisible(true);
+
 		 try {
 			  DatabaseFactory data=new DatabaseFactoryImpl();
 			  LocateRegistry.createRegistry(PORT); 
 			  java.rmi.Naming.rebind("rmi://172.26.27.105:1099/data",  data);
+			  field.setText("Ready!");
 	          System.out.println("ready...");
 	       } catch (Exception e) {
 	            e.printStackTrace();
