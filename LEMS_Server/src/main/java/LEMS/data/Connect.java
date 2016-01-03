7e4e11d2e248633ab66126c5f80b5fa4b42656be
zhangchengyao <tc2002618@126.com>
2016-01-03 23:03:10 +0800

    #09 ==-==

--- LEMS_Server/src/main/java/LEMS/data/Connect.java
+++ LEMS_Server/src/main/java/LEMS/data/Connect.java
@@ -6,9 +6,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import LEMS.init.GetIP;
+
 public class Connect {
 	public static final String DBDRIVER = "org.gjt.mm.mysql.Driver";
-	public static final String DBURL = "jdbc:mysql://172.25.135.175:3306/mldn";
+	public static final String DBURL = "jdbc:mysql:"+GetIP.IP+":3306/mldn";
 	public static final String DBUSER = "root";
 	public static final String DBPASS = "admin";
 
