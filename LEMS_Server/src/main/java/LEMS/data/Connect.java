d34c4d92c78a298240a81e6b45189497e34a9ec8
zhangchengyao <tc2002618@126.com>
2016-01-03 23:07:19 +0800

    #10 add//

--- LEMS_Server/src/main/java/LEMS/data/Connect.java
+++ LEMS_Server/src/main/java/LEMS/data/Connect.java
@@ -10,7 +10,7 @@ import LEMS.init.GetIP;
 
 public class Connect {
 	public static final String DBDRIVER = "org.gjt.mm.mysql.Driver";
-	public static final String DBURL = "jdbc:mysql:"+GetIP.IP+":3306/mldn";
+	public static final String DBURL = "jdbc:mysql://"+GetIP.IP+":3306/mldn";
 	public static final String DBUSER = "root";
 	public static final String DBPASS = "admin";
 
